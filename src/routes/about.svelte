124e7512465e1518252b9f39c433dac05f265fd3
Brian Ketelsen <mail@bjk.fyi>
2020-10-02 08:26:20 -0400

    fix build

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,27 @@
+<script context="module">
+  export async function preload({ params }) {
+    // the `slug` parameter is available because
+    // this file is called [slug].svelte
+    const res = await this.fetch(`/api/message`);
+    const data = await res.json();
+
+    if (res.status === 200) {
+      return { message: data };
+    } else {
+      this.error(res.status, data.message);
+    }
+  }
+</script>
+
+<script>
+  export let message;
+</script>
+
 <svelte:head>
-	<title>About</title>
+  <title>About</title>
 </svelte:head>
 
 <h1>About this site</h1>
 
 <p>This is the 'about' page. There's not much here.</p>
+<p>Message: {message.text}</p>
